a645a5b04154b5e47395f307123fe8f174846d37
MD JIBON HOWLADER <ProgrammerJibon@gmail.com>
2021-11-06 14:52:45 +0600

    Added permission request from java

--- app/src/main/java/bd/com/jibon/AUScoreboard/Splash.java
+++ app/src/main/java/bd/com/jibon/AUScoreboard/Splash.java
@@ -5,12 +5,14 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.security.Permission;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,12 +24,10 @@ public class Splash extends AppCompatActivity {
             Manifest.permission.ACCESS_NETWORK_STATE,
             Manifest.permission.ACCESS_WIFI_STATE,
             Manifest.permission.VIBRATE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
-    int PERMISSIONS_ALL = 125;
+    int PERMISSIONS_ALL = 1;
     public Activity activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +35,28 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         activity = this;
 
-        if (hasPermission(activity)){
+        if (hasPermission(activity, PERMISSIONS)){
             runMain();
         }else{
             ActivityCompat.requestPermissions(activity, PERMISSIONS, PERMISSIONS_ALL);
+            new CustomTools(activity).toast("Give permission to Vibrate, Location and Internet...", R.drawable.ic_baseline_warning_24);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        boolean results = true;
-        for (int grantResult : grantResults) {
-            if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                results = false;
-                break;
+        Boolean inter = true;
+        for (String permission : permissions){
+            if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED){
+                Log.e("errnos", permission+" not granted");
+                inter = false;
             }
         }
-        if (results){
+        if (inter){
             runMain();
         }else{
-            new CustomTools(activity).toast("We need Vibrate and Internet permission...", R.drawable.ic_baseline_warning_24);
+            new CustomTools(activity).toast("We need Vibrate, Location and Internet permission...", R.drawable.ic_baseline_warning_24);
             finish();
         }
     }
@@ -68,12 +69,10 @@ public class Splash extends AppCompatActivity {
             }
         }, 1500);
     }
-    public boolean hasPermission(Activity activity){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity != null){
-            for (String permission : PERMISSIONS){
-                if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED){
-                    return false;
-                }
+    public static boolean hasPermission(Activity activity, String... PERMISSIONS){
+        for (String permission : PERMISSIONS){
+            if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
             }
         }
         return true;
